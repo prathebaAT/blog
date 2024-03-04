029a5617fac829f7f4e7117fe10568e743dab9fb
prathebaAT <pratheba63@gmail.com>
2024-03-04 19:07:13 +0530

    Added jpa repository

---
+++ src/main/java/com/sampleblog/blog/BlogRepository.java
@@ -0,0 +1,12 @@
+package com.sampleblog.blog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<BlogBean,Long>{
+
+    
+} 
+    
+
