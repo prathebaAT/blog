eedbe69c695346671554e3ef902db4e00247fe19
prathebaAT <pratheba63@gmail.com>
2024-03-06 19:29:32 +0530

    added viewblogs file

--- src/main/java/com/sampleblog/blog/BlogController.java
+++ src/main/java/com/sampleblog/blog/BlogController.java
@@ -3,12 +3,15 @@ package com.sampleblog.blog;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
 
 
 
@@ -29,6 +32,12 @@ public class BlogController {
         // Add flash attribute to indicate successful saving
         redirectAttributes.addFlashAttribute("message", "Blog saved successfully");
         return "redirect:/index";
+    }
+    @RequestMapping("/view-blogs")
+    public String viewBlogs(Model model) {
+        // Fetch and display all blogs
+        model.addAttribute("blogs", blogRepository.findAll());
+        return "view-blogs";
     }
     
 }
