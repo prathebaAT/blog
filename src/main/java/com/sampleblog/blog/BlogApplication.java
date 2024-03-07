53a20b5fd8af2cbfb81b67507ed0bebe41631398
prathebaAT <pratheba63@gmail.com>
2024-03-07 19:36:38 +0530

    added retrieving file

--- src/main/java/com/sampleblog/blog/BlogApplication.java
+++ src/main/java/com/sampleblog/blog/BlogApplication.java
@@ -1,13 +1,23 @@
 package com.sampleblog.blog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class BlogApplication {
+	    
+	@Autowired
+    private DataRetriever dataRetriever;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+	@PostConstruct
+    public void init() {
+        dataRetriever.printAllBlogs();
+    }
 
 }
